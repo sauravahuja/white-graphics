730a571bd98f382b4386e7a5e928188408d0721b
Saurav Ahuja <sauravahuja11@gmail.com>
2019-12-14 13:12:41 +0530

    Till Testimonial Section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
  * scripts.js
  * Contains Script for basic static website named "White Graphics"
 **/
+
+$('.works-wrapper').magnificPopup({
+    delegate:'a',
+    type: 'image'
+});
+
+$(document).ready(function(){
+    $('#team-members').owlCarousel({
+        autoplay: 2000,
+        items: 3,
+        loop: 'true',
+        smartSpeed: 700,
+        margin: 30
+    });
+});
+
+$(document).ready(function(){
+    $("#customer-reviews").owlCarousel({
+        autoplay: 2000,
+        items: 1,
+        loop: 'true',
+        smartSpeed: 700
+    });
+});
